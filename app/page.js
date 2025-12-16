a3809aa9670e3287a7b311b8f2efb1574a7cbdab
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-12-16 16:43:05 +0100

    commit 2

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>Oliwers test sida</h1>
+          <h1>Oliwers test sida 2</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
